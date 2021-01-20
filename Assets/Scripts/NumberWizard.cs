1be8bb2d6384adf278e682325f15de9cc544aa94
Hudson Lessa <admin@hudsonlessa.com>
2021-01-20 14:21:02 -0300

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    public void OnPressLower() {
+        max = guess - 1;
+        GuessNext();
+    }
+
+    public void OnPressHigher() {
+        min = guess + 1;
+        GuessNext();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        GuessNext();
+    }
+
+    void GuessNext()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
